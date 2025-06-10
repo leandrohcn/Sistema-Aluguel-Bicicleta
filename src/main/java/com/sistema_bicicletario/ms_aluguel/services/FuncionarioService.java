a3f05a6d3355481f496a99d2d631bebb7d210d13
leandrohcn <leandro1111bfr@gmail.com>
2025-06-10 17:02:40 -0300

    fix: nome e assinatura dos métodos de Service, retirada dos ResponseEntity.

--- src/main/java/com/sistema_bicicletario/ms_aluguel/services/FuncionarioService.java
+++ src/main/java/com/sistema_bicicletario/ms_aluguel/services/FuncionarioService.java
@@ -14,7 +14,7 @@ public class FuncionarioService {
         this.funcionarioRepository = funcionarioRepository;
     }
 
-    public FuncionarioEntity addFuncionario(NovoFuncionarioDTO funcionarioDTO) {
+    public FuncionarioEntity criaFuncionario(NovoFuncionarioDTO funcionarioDTO) {
         FuncionarioEntity funcionario = new FuncionarioEntity();
         funcionario.setNome(funcionarioDTO.getNome());
         funcionario.setCpf(funcionarioDTO.getCpf());
@@ -28,22 +28,20 @@ public class FuncionarioService {
         return new FuncionarioEntity(funcionarioNovo);
     }
 
-    public ResponseEntity<FuncionarioEntity> atualizaFuncionario(NovoFuncionarioDTO funcionarioDTO,
-                                                                 Long idFuncionario) {
+    public void atualizaFuncionario(NovoFuncionarioDTO funcionarioDTO,
+                                    Long idFuncionario) {
 
-        return funcionarioRepository.findById(idFuncionario).map(funcionarioEntity -> {
-                funcionarioEntity.setCpf(funcionarioDTO.getCpf() != null ? funcionarioDTO.getCpf() : funcionarioEntity.getCpf());
-                funcionarioEntity.setNome(funcionarioDTO.getNome() != null ? funcionarioDTO.getNome() : funcionarioEntity.getNome());
-                funcionarioEntity.setEmail(funcionarioDTO.getEmail() != null ? funcionarioDTO.getEmail() : funcionarioEntity.getEmail());
-                funcionarioEntity.setSenha(funcionarioDTO.getSenha() != null ? funcionarioDTO.getSenha() : funcionarioEntity.getSenha());
-                funcionarioEntity.setConfirmaSenha(funcionarioDTO.getConfirmaSenha() != null ? funcionarioDTO.getConfirmaSenha() : funcionarioEntity.getConfirmaSenha());
-                funcionarioEntity.setIdade(funcionarioDTO.getIdade() > 0 ? funcionarioDTO.getIdade() : funcionarioEntity.getIdade());
+        funcionarioRepository.findById(idFuncionario).map(funcionarioEntity -> {
+            funcionarioEntity.setCpf(funcionarioDTO.getCpf() != null ? funcionarioDTO.getCpf() : funcionarioEntity.getCpf());
+            funcionarioEntity.setNome(funcionarioDTO.getNome() != null ? funcionarioDTO.getNome() : funcionarioEntity.getNome());
+            funcionarioEntity.setEmail(funcionarioDTO.getEmail() != null ? funcionarioDTO.getEmail() : funcionarioEntity.getEmail());
+            funcionarioEntity.setSenha(funcionarioDTO.getSenha() != null ? funcionarioDTO.getSenha() : funcionarioEntity.getSenha());
+            funcionarioEntity.setConfirmaSenha(funcionarioDTO.getConfirmaSenha() != null ? funcionarioDTO.getConfirmaSenha() : funcionarioEntity.getConfirmaSenha());
+            funcionarioEntity.setIdade(funcionarioDTO.getIdade() > 0 ? funcionarioDTO.getIdade() : funcionarioEntity.getIdade());
 
-
-                funcionarioRepository.save(funcionarioEntity);
-                return ResponseEntity.ok(funcionarioEntity);
-        }
-        ).orElse(ResponseEntity.notFound().build());
+            funcionarioRepository.save(funcionarioEntity);
+            return funcionarioEntity;
+        });
     }
 
     public void excluiFuncionario(Long idFuncionario) {
