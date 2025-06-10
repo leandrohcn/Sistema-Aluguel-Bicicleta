57c80d0bab6e32c48b8f1e95689f4d454cbdb066
leandrohcn <leandro1111bfr@gmail.com>
2025-06-10 17:15:35 -0300

    fix: Retirada do repository no controller, nomenclatura em pt-br, fim da ambiguidade nas anotações. Falta tratamento de erros.

--- src/main/java/com/sistema_bicicletario/ms_aluguel/controllers/FuncionarioController.java
+++ src/main/java/com/sistema_bicicletario/ms_aluguel/controllers/FuncionarioController.java
@@ -2,41 +2,52 @@ package com.sistema_bicicletario.ms_aluguel.controllers;
 
 import com.sistema_bicicletario.ms_aluguel.dtos.NovoFuncionarioDTO;
 import com.sistema_bicicletario.ms_aluguel.entitys.funcionario.FuncionarioEntity;
-import com.sistema_bicicletario.ms_aluguel.repositorys.FuncionarioRepository;
 import com.sistema_bicicletario.ms_aluguel.services.FuncionarioService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+
 @RestController
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
-    @Autowired
-    private FuncionarioRepository funcionarioRepository;
-    @Autowired
-    private FuncionarioService funcionarioService;
+
+    private final FuncionarioService funcionarioService;
+
+    public FuncionarioController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
 
     @GetMapping
-    public ResponseEntity<List<FuncionarioEntity>> getAllFuncionarios() {
-        return new ResponseEntity<>(funcionarioRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<FuncionarioEntity>> buscaTodosFuncionarios() {
+        try{
+            List<FuncionarioEntity> funcionarios = funcionarioService.buscaTodosFuncionario();
+            return new ResponseEntity<>(funcionarios, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping
-    public ResponseEntity<FuncionarioEntity> addFuncionario(@RequestBody NovoFuncionarioDTO funcionario) {
-        FuncionarioEntity response = funcionarioService.addFuncionario(funcionario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    public ResponseEntity<FuncionarioEntity> criaFuncionario(@RequestBody NovoFuncionarioDTO funcionario) {
+        try {
+            FuncionarioEntity response = funcionarioService.criaFuncionario(funcionario);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<FuncionarioEntity> getFuncionarioById(@PathVariable Long id) {
-        return funcionarioRepository.findById(id)
-                .map(funcionario -> ResponseEntity.ok(new FuncionarioEntity()))
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<FuncionarioEntity> buscaFuncionarioPorId(@PathVariable Long id) {
+        try {
+            FuncionarioEntity response = funcionarioService.buscaFuncionarioPorId(id);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PutMapping("/{id}")
