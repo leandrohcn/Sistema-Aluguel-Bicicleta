be8cf28d206e5e27ba06bc54adac67043d0c5787
leandrohcn <leandro1111bfr@gmail.com>
2025-06-12 10:26:37 -0300

    fix: mudanças referentes á construção dos métodos criaFuncionario e AtualizaFuncionario

--- src/main/java/com/sistema_bicicletario/ms_aluguel/services/FuncionarioService.java
+++ src/main/java/com/sistema_bicicletario/ms_aluguel/services/FuncionarioService.java
@@ -16,23 +16,21 @@ public class FuncionarioService {
     }
 
     public FuncionarioEntity criaFuncionario(NovoFuncionarioDTO funcionarioDTO) {
-        FuncionarioEntity funcionario = new FuncionarioEntity();
-        funcionario.setNome(funcionarioDTO.getNome());
-        funcionario.setCpf(funcionarioDTO.getCpf());
-        funcionario.setEmail(funcionarioDTO.getEmail());
-        funcionario.setSenha(funcionarioDTO.getSenha());
-        funcionario.setConfirmaSenha(funcionarioDTO.getConfirmaSenha());
-        funcionario.setIdade(funcionarioDTO.getIdade());
-        funcionario.setFuncao(funcionarioDTO.getFuncao());
-        FuncionarioEntity funcionarioNovo = funcionarioRepository.save(funcionario);
-
-        return new FuncionarioEntity(funcionarioNovo);
+        FuncionarioEntity funcionario = new FuncionarioEntity(
+                funcionarioDTO.getNome(),
+                funcionarioDTO.getSenha(),
+                funcionarioDTO.getEmail(),
+                funcionarioDTO.getIdade(),
+                funcionarioDTO.getCpf(),
+                funcionarioDTO.getFuncao()
+        );
+        return funcionarioRepository.save(funcionario);
     }
 
-    public void atualizaFuncionario(NovoFuncionarioDTO funcionarioDTO,
+    public FuncionarioEntity atualizaFuncionario(NovoFuncionarioDTO funcionarioDTO,
                                     Long idFuncionario) {
 
-        funcionarioRepository.findById(idFuncionario).map(funcionarioEntity -> {
+        return funcionarioRepository.findById(idFuncionario).map(funcionarioEntity -> {
             funcionarioEntity.setCpf(funcionarioDTO.getCpf() != null ? funcionarioDTO.getCpf() : funcionarioEntity.getCpf());
             funcionarioEntity.setNome(funcionarioDTO.getNome() != null ? funcionarioDTO.getNome() : funcionarioEntity.getNome());
             funcionarioEntity.setEmail(funcionarioDTO.getEmail() != null ? funcionarioDTO.getEmail() : funcionarioEntity.getEmail());
@@ -40,9 +38,9 @@ public class FuncionarioService {
             funcionarioEntity.setConfirmaSenha(funcionarioDTO.getConfirmaSenha() != null ? funcionarioDTO.getConfirmaSenha() : funcionarioEntity.getConfirmaSenha());
             funcionarioEntity.setIdade(funcionarioDTO.getIdade() > 0 ? funcionarioDTO.getIdade() : funcionarioEntity.getIdade());
 
-            funcionarioRepository.save(funcionarioEntity);
-            return funcionarioEntity;
-        });
+           return funcionarioRepository.save(funcionarioEntity);
+
+        }).orElseThrow(() -> new RuntimeException("Funcionario não encontrado com ID: " + idFuncionario));
     }
 
     public void excluiFuncionario(Long idFuncionario) {
