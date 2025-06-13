7ff18dde0fd73c65e4bf867307d705e02a05c489
leandrohcn <leandro1111bfr@gmail.com>
2025-06-12 23:16:04 -0300

    fix: Implementação de tratamento de Erros e Exceções

---
+++ src/main/java/com/sistema_bicicletario/ms_aluguel/exceptions/TrataUnprocessabeEntity.java
@@ -0,0 +1,9 @@
+package com.sistema_bicicletario.ms_aluguel.exceptions;
+
+public class TrataUnprocessabeEntity extends RuntimeException {
+    public TrataUnprocessabeEntity(String mensagem) {
+        super(mensagem);
+    }
+}
+
+
---
+++ src/main/java/com/sistema_bicicletario/ms_aluguel/exceptions/TratamentoDeErros.java
@@ -0,0 +1,40 @@
+package com.sistema_bicicletario.ms_aluguel.exceptions;
+
+import com.sistema_bicicletario.ms_aluguel.dtos.ErroDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class TratamentoDeErros {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErroDTO>> handleValidacao(MethodArgumentNotValidException ex) {
+        List<ErroDTO> erros = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(erro -> new ErroDTO(erro.getField(), erro.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class, RuntimeException.class})
+    public ResponseEntity<ErroDTO> handleNotFound(RuntimeException ex) {
+        ErroDTO erro = new ErroDTO("erro", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(TrataUnprocessabeEntity.class)
+    public ResponseEntity<ErroDTO> handleRegraNegocio(TrataUnprocessabeEntity ex) {
+        ErroDTO erro = new ErroDTO("Requisição inválida", ex.getMessage());
+        return ResponseEntity.status(422).body(erro);
+    }
+
+}
