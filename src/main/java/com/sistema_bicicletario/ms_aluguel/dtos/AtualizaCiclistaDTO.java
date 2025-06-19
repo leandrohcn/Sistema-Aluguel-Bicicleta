0435e10c91191b5fadcb388c7fae6e1a00b67877
leandrohcn <leandro1111bfr@gmail.com>
2025-06-19 14:31:10 -0300

    fix: retirada do final pra funcionalidade do test

--- src/main/java/com/sistema_bicicletario/ms_aluguel/dtos/AtualizaCiclistaDTO.java
+++ src/main/java/com/sistema_bicicletario/ms_aluguel/dtos/AtualizaCiclistaDTO.java
@@ -5,25 +5,26 @@ import com.sistema_bicicletario.ms_aluguel.entities.ciclista.Nacionalidade;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 
-@Getter @AllArgsConstructor @Setter
+@Getter @AllArgsConstructor @Setter @NoArgsConstructor
 public class AtualizaCiclistaDTO {
 
-    private final String nome;
-    private final String senha;
-    private final String confirmaSenha;
+    private String nome;
+    private String senha;
+    private String confirmaSenha;
     @JsonFormat(pattern = "dd/MM/yyyy")
-    private final LocalDate dataNascimento;
-    private final String cpf;
-    private final Nacionalidade nacionalidade;
+    private LocalDate dataNascimento;
+    private String cpf;
+    private Nacionalidade nacionalidade;
     @Email
-    private final String email;
-    private final String urlFotoDocumento;
-    private final PassaporteDTO passaporte;
+    private String email;
+    private String urlFotoDocumento;
+    private PassaporteDTO passaporte;
 
     public boolean senhaValida() {
         if (senha == null || confirmaSenha == null) {
