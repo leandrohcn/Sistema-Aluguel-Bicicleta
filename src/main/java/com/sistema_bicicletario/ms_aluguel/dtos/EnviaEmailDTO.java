baff31df2404cbd79bbc91d3aec6af25d91107db
leandrohcn <leandro1111bfr@gmail.com>
2025-07-01 04:44:52 -0300

    fix: Dtozin de envia email

--- src/main/java/com/sistema_bicicletario/ms_aluguel/dtos/EnviaEmailDTO.java
+++ src/main/java/com/sistema_bicicletario/ms_aluguel/dtos/EnviaEmailDTO.java
@@ -1,13 +1,13 @@
-//package com.sistema_bicicletario.ms_aluguel.dtos;
-//
-//import lombok.Data;
-//
-//@Data
-//public class EnviaEmailDTO {
-//    private String mensagem;
-//    private String email;
-//
-//    public void envioDeMensagem(String email) {
-//        mensagem = "Sucesso ao enviar mensagem para email: " + email;
-//    }
-//}
+package com.sistema_bicicletario.ms_aluguel.dtos;
+
+import lombok.Data;
+
+@Data
+public class EnviaEmailDTO {
+    private String mensagem;
+    private String email;
+
+    public void envioDeEmail() {
+        mensagem = "Sucesso ao enviar mensagem para email: " + email;
+    }
+}
